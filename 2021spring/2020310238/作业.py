df49be37cafdb11ad99261c5e03c52c052cb02f9
Doris-osh <81304060+Doris-osh@users.noreply.github.com>
2021-03-31 19:57:51 +0800

    Update 作业.py

--- 2021spring/2020310238/作业.py
+++ 2021spring/2020310238/作业.py
@@ -1,3 +1,4 @@
+'''
 def fibe(n):
     s = [1,1]
     while not s[-1] > n:
@@ -10,5 +11,19 @@ def fibe(n):
     return s1
 n = int(input('请输入正整数n：'))
 print(str(n)+'以内的斐波那契数列为：',end = '')
+print(fibe(n)[0],end = '')
+print('\n这些斐波那契数的乘积为：'+str(fibe(n)[1]))
+'''
+
+def fibe(n):
+    s = [1,1]
+    t = 1
+    for i in range(n-2):
+        s.append(s[i]+s[i+1])
+        t *= s[-1]
+    s1 = [s,t]
+    return s1
+n = int(input('请输入正整数n：'))
+print('前'+str(n)+'个斐波那契数为：',end = '')
 print(fibe(n)[0],end = '')
 print('\n这些斐波那契数的乘积为：'+str(fibe(n)[1]))
