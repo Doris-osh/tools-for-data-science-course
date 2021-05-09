555ead85f6dd93644d0fc69d8644958ea7d3fd89
Doris-osh <81304060+Doris-osh@users.noreply.github.com>
2021-05-09 17:45:22 +0800

    Add files via upload

---
+++ 2021spring/2020310238/wordclud.py
@@ -0,0 +1,40 @@
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import jieba
+from wordcloud import WordCloud, STOPWORDS
+
+
+###当前文件路径
+d = path.dirname(__file__)
+
+# Read the whole text.
+file = open(path.join(d, 'alice.txt'),encoding='utf-8').read()
+##进行分词
+#刚开始是分完词放进txt再打开却总是显示不出中文很奇怪
+default_mode =jieba.cut(file)
+text = " ".join(default_mode)
+alice_mask = np.array(Image.open(path.join(d, "mmexport1566476329183.jpg")))
+stopwords = set(STOPWORDS)
+stopwords.add("said")
+wc = WordCloud(  
+    #设置字体，不指定就会出现乱码,这个字体文件需要下载
+    font_path=r'C:\字体\良怀行书.ttf',  
+    background_color="white",   
+    max_words=2000,   
+    mask=alice_mask,  
+    stopwords=stopwords)  
+# generate word cloud
+wc.generate(text)
+
+# store to file
+wc.to_file(path.join(d, "result.jpg"))
+
+# show
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.figure()
+plt.imshow(alice_mask, cmap=plt.cm.gray, interpolation='bilinear')
+plt.axis("off")
+plt.show()
